2b06c9b17865d17bf150a77b6259b6418a590c3a
Ahmadziad121 <ahmadziad758@gmail.com>
2024-02-19 13:45:43 +0300

    Create Car.js

---
+++ reactlogin/src/Car.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+class Car extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={}
+    }
+    render()
+    {
+        return(
+            <div className="CAR" >
+            
+            </div>
+        )
+    }
+}
+export default Car;
